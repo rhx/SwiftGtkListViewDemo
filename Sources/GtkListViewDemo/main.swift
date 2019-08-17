020edd56ede6abd3d1a40a398affd17272328199
Rene Hexel <r.hexel@griffith.edu.au>
2019-08-18 06:31:09 +1000

    s/var/let/g

--- Sources/GtkListViewDemo/main.swift
+++ Sources/GtkListViewDemo/main.swift
@@ -3,14 +3,14 @@ import CGtk
 import Gtk
 
 let status = Application.run { app in
-    var window = ApplicationWindowRef(application: app)
+    let window = ApplicationWindowRef(application: app)
     window.title = "Book List Demo"
     window.setDefaultSize(width: 640, height: 360)
 
     let listView: ListView
     do {
         let store = ListStore(.string, .string, .boolean)
-        var i = TreeIter()
+        let i = TreeIter()
         store.append(asNextRow: i, "The Principle of Reason", "Martin Heidegger", false)
         store.append(asNextRow: i, "The Art of Computer Programming", "Donald E. Knuth", true)
         store.append(asNextRow: i, "Structure and Interpretation of Computer Programs", "Harold Abelson and Gerald J. Sussman", false)
