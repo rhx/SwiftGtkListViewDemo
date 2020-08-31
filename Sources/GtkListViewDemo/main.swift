700ccc4eb56bd7539b064c756a7a9ceaf9d671cf
Rene Hexel <r.hexel@griffith.edu.au>
2020-09-01 06:48:26 +1000

    Upgrade to Swift 5.2 and gir2swift 11

--- Sources/GtkListViewDemo/main.swift
+++ Sources/GtkListViewDemo/main.swift
@@ -2,25 +2,32 @@ import GLibObject
 import CGtk
 import Gtk
 
-let status = Application.run { app in
-    let window = ApplicationWindowRef(application: app)
+var window: ApplicationWindow!
+var store: ListStore!
+var listView: ListView!
+var renderers = [CellRenderer]()
+var columns = [TreeViewColumn]()
+var i = TreeIter()
+
+let status = Application.run(startupHandler: nil) { app in
+    window = ApplicationWindow(application: app)
     window.title = "Book List Demo"
     window.setDefaultSize(width: 640, height: 360)
 
-    let listView: ListView
     do {
-        let store = ListStore(.string, .string, .boolean)
-        let i = TreeIter()
+        i = TreeIter()
+        store = ListStore(.string, .string, .boolean)
         store.append(asNextRow: i, "The Principle of Reason", "Martin Heidegger", false)
         store.append(asNextRow: i, "The Art of Computer Programming", "Donald E. Knuth", true)
         store.append(asNextRow: i, "Structure and Interpretation of Computer Programs", "Harold Abelson and Gerald J. Sussman", false)
         listView = ListView(model: store)
-        let columns = [
+        columns = [
             ("Author", "text", CellRendererText()),
             ("Title",  "text", CellRendererText()),
             ("Lent", "active", CellRendererToggle())].enumerated().map {
                 (i: Int, c: (title: String, kind: PropertyName, renderer: CellRenderer)) -> TreeViewColumn in
-                var column = TreeViewColumn(i, title: c.title, renderer: c.renderer, attribute: c.kind)
+                renderers.append(c.renderer)
+                let column = TreeViewColumn(i, title: c.title, renderer: c.renderer, attribute: c.kind)
                 return column
         }
         listView.append(columns)
@@ -29,9 +36,11 @@ let status = Application.run { app in
     window.showAll()
 }
 
-guard let status = status else {
-    fatalError("Could not create Application")
-}
-guard status == 0 else {
-    fatalError("Application exited with status \(status)")
+withExtendedLifetime([window as Any, store as Any, columns, renderers, listView as Any, i]) {
+    guard let status = status else {
+        fatalError("Could not create Application")
+    }
+    guard status == 0 else {
+        fatalError("Application exited with status \(status)")
+    }
 }
