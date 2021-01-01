7daff32c4e49e77d77140e9613e035a9ecbe10be
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-02 09:30:11 +1000

    Use main branch as default

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "GtkListViewDemo",
     dependencies: [
-        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("master")),
+        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("main")),
     ],
     targets: [
         .target(name: "GtkListViewDemo", dependencies: ["Gtk"]),
--- config.sh
+++ config.sh
@@ -22,9 +22,9 @@ GOBJECT_LIBDIR=`pkg-config --libs-only-L gobject-introspection-1.0 2>/dev/null |
 LINKFLAGS=`pkg-config --libs gtk+-$ver gdk-$ver pangocairo pangoft2 pango gobject-2.0 gio-unix-2.0 glib-2.0 | sed -e 's/ *--export-dynamic */ /g' -e 's/ *-Wl, */ /g' -e 's/,/ -Xlinker /g' -e 's/-pthread/-lpthread/g' -e 's/  */ /g' -e 's/^ *//' -e 's/ *$//' | tr ' ' '\n' | tr '	' '\n' | sed -e 's/^/-Xlinker /' | tr '\n' ' ' | sed -e 's/^/-Xlinker /' -e 's/-Xlinker *-Xlinker/-Xlinker/g' -e 's/-Xlinker[ 	]*-Xlinker/-Xlinker/g' -e 's/-Xlinker *$//'`" -Xlinker -L/usr/local/lib"
 CCFLAGS=`pkg-config --cflags gtk+-$ver gdk-$ver pangocairo pangoft2 pango gobject-2.0 gio-unix-2.0 glib-2.0 | sed -e 's/ *-Wl, */ /g' -e 's/,/ -Xlinker /g' -e 's/ *-pthread */ /g' -e 's/ *--export-dynamic */ /g' -e 's/  */ /g' -e 's/^ *//' -e 's/ *$//' | tr ' ' '\n' | tr '	' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' | sed -e 's/-Xlinker[ 	]*-Xlinker/-Xlinker/g' -e 's/-Xcc *$//'`
 TAC="tail -r"
-if which tac >/dev/null ; then
+if which tac >/dev/null 2>&1 ; then
    TAC=tac
-   else if which gtac >/dev/null ; then
+   else if which gtac >/dev/null 2>&1 ; then
 	TAC=gtac
    fi
 fi
