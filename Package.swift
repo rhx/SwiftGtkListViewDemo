3b667503b4b432747ca989a87efff323eee53a48
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-02 10:32:57 +1000

    Use gtk3 branch for gtk+-3.x

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "GtkListViewDemo",
     dependencies: [
-        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("main")),
+        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("gtk3")),
     ],
     targets: [
         .target(name: "GtkListViewDemo", dependencies: ["Gtk"]),
