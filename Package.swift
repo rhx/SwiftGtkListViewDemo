e1ef9b3cbad364d2102eb56c21ea019ea5d493a7
Rene Hexel <rhx@users.noreply.github.com>
2021-01-15 09:40:30 +1000

    Add gir2swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "GtkListViewDemo",
     dependencies: [
+        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("development")),
         .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("gtk3")),
     ],
     targets: [
